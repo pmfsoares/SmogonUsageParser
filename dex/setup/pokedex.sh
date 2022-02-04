a2f1cb926374dcccfe6208f36b51bda3479f456f
Pedro Soares <p.soares1997@live.com.pt>
2022-02-04 00:20:45 +0000

    Add minification to pokedex.json

--- dex/setup/pokedex.sh
+++ dex/setup/pokedex.sh
@@ -5,5 +5,5 @@ url=https://raw.githubusercontent.com/smogon/pokemon-showdown/master/data/pokede
 curl ${url} > pokedex.js
 
 sed -i "1s/.*/let Pokedex = {/" pokedex.js
-echo "console.log(JSON.stringify(Pokedex));" >> pokedex.js
+echo "console.log(JSON.stringify(Pokedex, null, 0));" >> pokedex.js
 node pokedex.js > ../pokedex.json 
